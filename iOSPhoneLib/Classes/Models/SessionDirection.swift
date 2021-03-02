d97357046dd84f7afb13ef5560a9f9422f329a06
Chris Kontos <chris.kontos@wearespindle.com>
2021-03-02 01:02:26 +0100

    Added isIncoming and direction properties to session

---
+++ iOSPhoneLib/Classes/Models/SessionDirection.swift
@@ -0,0 +1,13 @@
+//
+//  SessionDirection.swift
+//  iOSPhoneLib
+//
+//  Created by Chris Kontos on 02/03/2021.
+//
+
+import Foundation
+
+public enum SessionDirection {
+    case inbound
+    case outbound
+}
