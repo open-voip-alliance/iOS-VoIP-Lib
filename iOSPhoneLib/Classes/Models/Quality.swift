50214c920a854215ecbab04c6a07a9d120f21082
Chris Kontos <chris.kontos@wearespindle.com>
2021-03-05 19:51:23 +0100

    refactored quality

---
+++ iOSPhoneLib/Classes/Models/Quality.swift
@@ -0,0 +1,13 @@
+//
+//  Quality.swift
+//  iOSPhoneLib
+//
+//  Created by Chris Kontos on 05/03/2021.
+//
+
+import Foundation
+
+public struct Quality{
+    public let average: Float
+    public let current: Float
+}
